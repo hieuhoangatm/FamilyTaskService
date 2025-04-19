e53807904a75b21082e704a52aa299a40cab0a1c
Hoang Dinh Hieu <hieuhoangatm@gmail.com>
2025-04-19 16:50:43 +0700

    update

--- src/main/java/com/ptit/mobie/taskfamily_service/dto/request/UserRequestDTO.java
+++ src/main/java/com/ptit/mobie/taskfamily_service/dto/request/UserRequestDTO.java
@@ -1,4 +1,18 @@
 package com.ptit.mobie.taskfamily_service.dto.request;
 
-public class UserCreatRequest {
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRequestDTO {
+    private String username;
+    private String email;
+    private String gender;
+    private String phone;
+    private String role;
+    private Integer totalPoints;
+    private String language;
 }
